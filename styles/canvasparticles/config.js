e41ce8824050e61a3794033a0e2bc3e7b00f4279
v1ll4n <188228725@qq.com>
2017-03-23 19:30:56 +0800

    new main page

---
+++ styles/canvasparticles/config.js
@@ -0,0 +1,15 @@
+		window.onload = function(){
+			var config = {
+				vx: 4,
+				vy: 4,
+				height: 2,
+				width: 2,
+				count: 120,
+				color: "74, 166, 172",
+				stroke: "74, 166, 172",
+				dist: 10000,
+				e_dist: 30000,
+				max_conn: 10
+			}
+			CanvasParticle(config);
+		}
